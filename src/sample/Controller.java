32786e063158e9d2d17d0194cbaf7265d4cdac9e
Sergi Barjola <sergibca@gmail.com>
2015-11-05 17:04:36 +0100

    Implementado el diseño básico de la aplicación. Algunos métodos y elementos básicos definidos.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,29 @@
 package sample;
 
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.image.ImageView;
+
+
 public class Controller {
+
+    public ListView<String> listaTiempo;
+    public ObservableList observableList;
+    public Button refreshButton;   // Botón que refresca la lista
+
+    public void initialize(){
+       refreshButton.setGraphic(new ImageView("/img/refresh.png"));
+       actualizarLista(null);  //Actualizamos la lista automaticamente al inicio
+    }
+
+    public void actualizarLista(ActionEvent actionEvent) {
+
+        //Leemos del XML con DOM
+
+        observableList.clear();
+
+        
+    }
 }
